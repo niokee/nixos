950124d18da67c737227f46608491f993969b410
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2024-04-21 14:27:08 +0200

    add general file to wezterm

--- home/.dotfiles/wezterm/wezterm.lua
+++ home/.dotfiles/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local Config = require('config')
 
 
 return Config:init()
+    :append(require("config.general"))
     :append(require("config.plugins"))
     :append(require("config.fonts"))
     :append(require('config.appearance')).options
