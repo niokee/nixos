ffceefe9299f241a5e2bc08c3550e967fe8ae87e
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2024-06-08 16:08:29 +0200

    add ruff for python in nvim

--- home/.dotfiles/nvim/after/plugin/none-ls.lua
+++ home/.dotfiles/nvim/after/plugin/none-ls.lua
@@ -4,8 +4,7 @@ none_ls.setup({
 	sources = {
 		none_ls.builtins.formatting.gofmt,
 		none_ls.builtins.formatting.goimports,
-		none_ls.builtins.formatting.isort,
-		none_ls.builtins.formatting.black,
+		none_ls.builtins.formatting.ruff,
 		none_ls.builtins.formatting.stylua,
 		none_ls.builtins.formatting.alejandra,
 		none_ls.builtins.completion.spell,
