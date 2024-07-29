5fdd7d172d6001bee261195c7c572e1a763a08b5
Mateusz Dziuba <mateusz.dziuba97@gmail.com>
2024-07-29 11:38:05 +0200

    Reapply "change which-key for after/which-key"
    This reverts commit 02dfa6025a95081119f6d3cba81fb41a915abd79.

--- home/.dotfiles/nvim/after/plugin/which-key.lua
+++ home/.dotfiles/nvim/after/plugin/which-key.lua
@@ -1,63 +1,47 @@
 local wk = require("which-key")
-wk.register({
-	name = "File switching",
-	["<leader>pv"] = { "<cmd>Neotree filesystem reveal float<CR>", "Open file tree" },
-}, {
-	mode = "n",
+wk.add({
+    { "<leader>pv", "<cmd>Neotree filesystem reveal float<CR>", desc = "Open file tree" },
+    mode = { "n" },
 })
-wk.register({
-	name = "Line manipulation",
-	J = { ":m '>+1<CR>gv=gv", "Move line down" },
-	K = { ":m '<-2<CR>gv=gv", "Move line up" },
-}, {
-	mode = "v",
+wk.add({
+    { "J", ":m '>+1<CR>gv=gv", desc = "Move line down" },
+    { "K", ":m '<-2<CR>gv=gv", desc = "Move line up" },
+    mode = { "v" },
 })
-wk.register({
-	name = "Movement",
-	n = { "nzzzv", "Move to next search result" },
-	N = { "Nzzzv", "Move to previous search result" },
-	["<c-d>"] = { "<C-d>zz", "Move down half page" },
-	["<c-u>"] = { "<C-u>zz", "Move up half page" },
-	["<M-j>"] = { "<cmd>cprev<CR>zz", "Move to previous quickfix result" },
-	["<M-k>"] = { "<cmd>cnext<CR>zz", "Move to next quickfix result" },
-	["<leader>"] = {
-		k = { "<cmd>lnext<CR>zz", "Move to next location list result" },
-		j = { "<cmd>lprev<CR>zz", "Move to previous location list result" },
-	},
+wk.add({
+    { "n",         "nzzzv",            desc = "Move to next search result" },
+    { "N",         "Nzzzv",            desc = "Move to previous search result" },
+    { "<c-d>",     "<C-d>zz",          desc = "Move down half page" },
+    { "<c-u>",     "<C-u>zz",          desc = "Move up half page" },
+    { "<M-j>",     "<cmd>cprev<CR>zz", desc = "Move to previous quickfix result" },
+    { "<M-k>",     "<cmd>cnext<CR>zz", desc = "Move to next quickfix result" },
+    { "<leader>k", "<cmd>lnext<CR>zz", desc = "Move to next location list result" },
+    { "<leader>j", "<cmd>lprev<CR>zz", desc = "Move to previous location list result" },
+    { "J",         "mzJ`z", desc = "Join lines" },
+    mode = { "n" },
 })
 
-wk.register({})
-wk.register({
-	name = "Yank and paste",
-	J = { "mzJ`z", "Join lines" },
-	["<leader>y"] = { [["+y]], "Yank to system clipboard" },
-	["<leader>Y"] = { [["+Y]], "Yank one line to system clipboard" },
-	["<leader>P"] = { [["_dP]], "Paste without yanking" },
-	["<leader>d"] = { [["_d]], "Delete without yanking" },
-}, {
-	mode = { "n", "v" },
+wk.add({
+    { "<leader>y", '"+y',   desc = "Yank to system clipboard" },
+    { "<leader>Y", '"+Y',   desc = "Yank one line to system clipboard" },
+    { "<leader>P", '"_dP',  desc = "Paste without yanking" },
+    { "<leader>d", '"_d',   desc = "Delete without yanking" },
+    mode = { "n", "v" },
 })
 
-wk.register({
-	name = "Insert mode mappings",
-	["<c-s>"] = { "<C-c>:update<CR>", "Save file" },
-	["<c-c>"] = { "<Esc>", "Exit insert mode" },
-}, {
-	mode = "i",
+wk.add({
+    { "<c-s>", "<C-c>:update<CR>", desc = "Save file" },
+    { "<c-c>", "<Esc>",            desc = "Exit insert mode" },
+    mode = { "i" },
 })
-wk.register({
-	name = "Misc",
-	["<c-f>"] = { "<cmd>silent !tmux neww tmux-sessionizer<CR>", "Open tmux sessionizer" },
-	["<leader>"] = {
-		s = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Replace word under cursor" },
-		x = { "<cmd>!chmod +x %<CR>", "Make file executable" },
-		["<leader>"] = { "<cmd>so<CR>", "Source file" },
-	},
-}, {
-	mode = "n",
+wk.add({
+    { "<c-f>",     "<cmd>silent !tmux neww tmux-sessionizer<CR>",          desc = "Open tmux sessionizer" },
+    { "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Replace word under cursor" },
+    { "<leader>x", "<cmd>!chmod +x %<CR>",                                 desc = "Make file executable" },
+    { "<leader><leader>",  "<cmd>so<CR>",                                          desc = "Source file" },
+    mode = { "n" },
 })
-wk.register({
-	["<c-s>"] = { "<C-c>:update<CR>", "Save file" },
-}, {
-	mode = { "v", "n" },
+wk.add({
+    { "<c-s>", "<C-c>:update<CR>", desc = "Save file" },
+    mode = { "v", "n" },
 })
